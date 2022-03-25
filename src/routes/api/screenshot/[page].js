aa96e955d6fdd264bbb84b05949a4073e8fe2cf8
Joshua L.J. Vachon <joshua.vachon@cyberfjord.studio>
2022-03-25 10:33:49 -0400

    Screenshot

---
+++ src/routes/api/screenshot/[page].js
@@ -0,0 +1,70 @@
+import puppeteer from "puppeteer-core";
+import chrome from "chrome-aws-lambda"
+
+const exePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+
+async function getOptions(isDev) {
+    let options;
+    if (isDev) {
+        options = {
+            args: [],
+            executablePath: exePath,
+            headless: true,
+        };
+    } else {
+        options = {
+            args: chrome.args,
+            executablePath: await chrome.executablePath,
+            headless: chrome.headless,
+        };
+    }
+    return options;
+}
+
+export async function get({url, params, res}) {
+    let statut, file, fileData
+
+    const uri = new URLSearchParams(url.search)
+    const isDev = uri.get("isDev") === "1"
+    const pause = uri.get("pause") || 0
+    const pageToScreenshot = params.page
+
+    try {
+        if (!pageToScreenshot.includes("https://")) {
+            statut = 404;
+        }
+
+        const options = await getOptions(isDev);
+
+        const browser = await puppeteer.launch(options);
+        const page = await browser.newPage();
+
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+            deviceScaleFactor: 1,
+        });
+
+        await page.goto(pageToScreenshot);
+        const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+        await delay(pause * 1000)
+       file = await page.screenshot({
+            type: "png",
+        });
+
+        fileData = "data:image/png;base64," + file.toString('base64')
+        await browser.close();
+
+        statut = 200;
+
+    } catch (e) {
+        statut = 500;
+    }
+
+    //const test1 = new Buffer(params.page);
+    //const test = test1.toString('base64')
+    return{
+        body: { fileData, statut },
+
+    }
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let url, temps = 0
+    let result
+    async function getParams(){
+        let rep = await fetch(`/api/screenshot/${encodeURIComponent(url)}?isDev=1&pause=${temps}`)
+        let s = await rep.json()
+        result = s.fileData
+        console.log(s.statut)
+    }
+
+</script>
+
+
+<img height="400" src="{result}">
+<br>
+<input bind:value={url} placeholder="URL" type="text"/>
+<input bind:value={temps} placeholder="Secondes avant?" type="text"/>
+<button on:click={getParams}>Faire un screenshot</button>
